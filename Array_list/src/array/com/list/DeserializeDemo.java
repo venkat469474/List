53fa071e7844f1f71d529c9045ffad2d4b3bbb0a
venkat <venkat@DESKTOP-SS5HA7D>
2017-06-07 00:30:53 +0530

    first commit

---
+++ Array_list/src/array/com/list/DeserializeDemo.java
@@ -0,0 +1,35 @@
+package array.com.list;
+
+	
+
+	import java.io.*;
+	public class DeserializeDemo {
+		
+ class Employee{
+	String name;
+	
+	
+}
+	   public static void main(String [] args) {
+	      Employee e = null;
+	      try {
+	         FileInputStream fileIn = new FileInputStream("/tmp/employee.ser");
+	         ObjectInputStream in = new ObjectInputStream(fileIn);
+	         e = (Employee) in.readObject();
+	         in.close();
+	         fileIn.close();
+	      }catch(IOException i) {
+	         i.printStackTrace();
+	         return;
+	      }catch(ClassNotFoundException c) {
+	         System.out.println("Employee class not found");
+	         c.printStackTrace();
+	         return;
+	      }
+	      
+	      System.out.println("Deserialized Employee...");
+	      System.out.println("Name: " + e.name);
+	      
+	   }
+	}
+
